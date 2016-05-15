e051891fca5121ee1ed41fec232ee8b079184910
jiahua <1094006351@qq.com>
2016-05-15 11:58:30 +0800

    code35没完成

---
+++ question35_Search_Insert_Position/SearchInsertPosition.java
@@ -0,0 +1,20 @@
+package zhang.algorithm.leetcode.question35_Search_Insert_Position;
+
+public class SearchInsertPosition {
+	/**
+	 * 
+	 * @param nums
+	 * @param target
+	 * @return
+	 */
+    public int searchInsert(int[] nums, int target) {
+        return 0;
+    }
+    
+    public static void main(String[] args){
+    	SearchInsertPosition test = new SearchInsertPosition();
+    	int[] nums = {};
+    	int target = 2;
+    	System.out.println( test.searchInsert(nums, target) );
+    }
+}
