2ada06acf668978c3c1c3c2ab0956aac9383d45a
jiahua <1094006351@qq.com>
2016-05-24 22:25:06 +0800

    新建code61的文件

---
+++ question61_Rotate_List/RotateList.java
@@ -0,0 +1,19 @@
+package zhang.algorithm.leetcode.question61_Rotate_List;
+
+public class RotateList {
+	
+    public ListNode rotateRight(ListNode head, int k) {
+        return null;
+    }
+	
+	public static void main(String[] args){
+		RotateList test = new RotateList();
+		System.out.println();
+	}
+}
+
+class ListNode {
+	int val;
+	ListNode next;
+	ListNode(int x) { val = x; }
+}
