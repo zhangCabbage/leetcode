ddee10c3bc8c7b0e3867e1ae8298f26faf5f6bc2
zhang_jiahua <1094006351@qq.com>
2016-06-26 20:25:33 +0800

    创建code130，但是没想出答案

---
+++ question130_Surrounded_Regions/SurroundedRegions.java
@@ -0,0 +1,26 @@
+package zhang.algorithm.leetcode.question130_Surrounded_Regions;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: zhang_Lenovo
+ * Date: 2016/6/26
+ * Time: 20:15
+ * To change this template use File | Settings | File Templates.
+ */
+public class SurroundedRegions {
+    /**
+     *
+     * X X X X           X X X X
+     * X O O X   ====>   X X X X
+     * X X O X   ====>   X X X X
+     * X O X X           X O X X
+     *
+     * How to solve this problem? I have no idea!!
+     * the hit tags is Bread first traversal and union find! But it same like useless.
+     *
+     * @param board
+     */
+    public void solve(char[][] board) {
+
+    }
+}
