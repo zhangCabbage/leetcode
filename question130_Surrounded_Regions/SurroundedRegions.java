487f34a2b6b60149506b12f45d21e0b3d10ac53f
zhang_jiahua <1094006351@qq.com>
2016-06-30 21:00:51 +0800

    update 130

--- question130_Surrounded_Regions/SurroundedRegions.java
+++ question130_Surrounded_Regions/SurroundedRegions.java
@@ -17,10 +17,44 @@ public class SurroundedRegions {
      *
      * How to solve this problem? I have no idea!!
      * the hit tags is Bread first traversal and union find! But it same like useless.
+     * This is about handle of graph
+     *
+     * Use breadth-first-traversal
      *
      * @param board
      */
     public void solve(char[][] board) {
+        if(board.length<3) return;
+        if(board[0].length<3) return;
+        for(int j=0; j<board[0].length; j++){
+            bfs(board, 0, j);
+            bfs(board, board.length-1, j);
+        }
+        for(int i=0; i<board.length; i++){
+            bfs(board, i, 0);
+            bfs(board, i, board[0].length-1);
+        }
+
+        for(int i=0; i<board.length; i++){
+            for(int j=0; j<board[0].length; j++){
+                if(board[i][j] == '#'){
+                    board[i][j] = 'O';
+                }else{
+                    board[i][j] = 'X';
+                }
+            }
+        }
+    }
+
+    private void bfs(char[][] board, int i, int j) {
+
+    }
+
+    /**
+     *
+     * @param board
+     */
+    public void solve2(char[][] board) {
 
     }
 }
