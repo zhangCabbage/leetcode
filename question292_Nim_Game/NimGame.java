a29d82f3483bc61d4b74c45150268ba7fe24dcae
jiahua <1094006351@qq.com>
2016-07-07 23:26:49 +0800

    update

--- question292_Nim_Game/NimGame.java
+++ question292_Nim_Game/NimGame.java
@@ -12,11 +12,13 @@ public class NimGame {
      * this problem is also very interesting!!
      * two side also can take one \ two \ three stone, and two side is also smart.
      * if I can finally take the last stone, I win.
-     *
+     * <p>
      * 分析后我们知道,当剩下四个的时候,谁先拿,谁死!,我们努力使得最后剩下四个!!
-     *
+     * 这是一个很有技巧的题,那么发散思维:当我们可以赢的时候,我们该如何拿石头才能赢呢?
+     * 第一次拿 n%4 (if n > 4 && n/4 != 0)个, 我们就能赢!
+     * <p>
      * Attention that this problem`s tag is Brainteaser(脑筋急转弯)!
-     *
+     * <p>
      * <strong>result of test:</strong><br/>
      * 60 / 60 test cases passed
      * Status: Accepted
@@ -26,6 +28,6 @@ public class NimGame {
      * @return
      */
     public boolean canWinNim(int n) {
-        return (n&3) != 0;
+        return (n & 3) != 0;
     }
 }
