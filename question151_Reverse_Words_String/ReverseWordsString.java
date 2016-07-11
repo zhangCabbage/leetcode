c1c41415c8192634c37315d1bc06c3f74a57c626
zhang_jiahua <1094006351@qq.com>
2016-07-11 20:59:12 +0800

    完成code151

--- question151_Reverse_Words_String/ReverseWordsString.java
+++ question151_Reverse_Words_String/ReverseWordsString.java
@@ -1,6 +1,6 @@
 package zhang.algorithm.leetcode.question151_Reverse_Words_String;
 
-import java.util.Collections;
+import java.util.regex.Pattern;
 
 /**
  * Created by IntelliJ IDEA.
@@ -10,9 +10,41 @@ import java.util.Collections;
  * To change this template use File | Settings | File Templates.
  */
 public class ReverseWordsString {
-
+    /**
+     * 22 / 22 test cases passed
+     * Status: Accepted
+     * Runtime: 12 ms, bit 40.88%
+     *
+     * @param s
+     * @return
+     */
     public String reverseWords(String s) {
-        String[] strs = s.split(" ");
+        //最后的分割符分割的不计，但是前面的分割符计
+        Pattern pattern = Pattern.compile(" +");
+        String[] strs = pattern.split(s);
 
+        int start = 0;
+        int end = strs.length - 1;
+        while (start < end) {
+            String temp = strs[start];
+            strs[start++] = strs[end];
+            strs[end--] = temp;
+        }
+
+        StringBuffer sb = new StringBuffer();
+        if (strs.length > 0) sb.append(strs[0]);
+        for (int i = 1; i < strs.length; i++) {
+            if (!strs[i].equals("")) {
+                sb.append(" ");
+                sb.append(strs[i]);
+            }
+        }
+
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        ReverseWordsString test = new ReverseWordsString();
+        System.out.println(test.reverseWords("  "));
     }
 }
