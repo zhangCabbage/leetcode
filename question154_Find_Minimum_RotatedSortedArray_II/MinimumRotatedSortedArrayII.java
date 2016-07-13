62cf14003b43ef69976ca63b760e8ea0b4526592
jiahua <1094006351@qq.com>
2016-07-13 11:31:28 +0800

    更新code153，完成code154和code198

---
+++ question154_Find_Minimum_RotatedSortedArray_II/MinimumRotatedSortedArrayII.java
@@ -0,0 +1,101 @@
+package zhang.algorithm.leetcode.question154_Find_Minimum_RotatedSortedArray_II;
+
+import java.util.Arrays;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: zhang_MacPro
+ * Date: 16/7/12
+ * Time: 下午9:32
+ * To change this template use File | Settings | File Templates.
+ */
+public class MinimumRotatedSortedArrayII {
+    /**
+     * But This way is too slow. Time complexity is O(n*logn)
+     * <strong>result of test:</strong><br/>
+     * 189 / 189 test cases passed
+     * Status: Accepted
+     * Runtime: 2 ms, bit 1.56%
+     *
+     * @param nums
+     * @return
+     */
+    public int findMin(int[] nums) {
+        Arrays.sort(nums);
+        return nums[0];
+    }
+
+    /**
+     * Traversal the int array. Time complexity is O(n)
+     * <p>
+     * <strong>result of test:</strong><br/>
+     * 189 / 189 test cases passed
+     * Status: Accepted
+     * Runtime: 1 ms, bit 4.88%
+     *
+     * @param nums
+     * @return
+     */
+    public int findMin2(int[] nums) {
+        int min = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (min > nums[i]) {
+                min = nums[i];
+            }
+        }
+        return min;
+    }
+
+    /**
+     * use the feature that it is a sorted rotate array. so if the pre one is bigger
+     * than the next one, so it is the answer.
+     * But it is almost the same as the last one.
+     *
+     * @param nums
+     * @return
+     */
+    public int findMin3(int[] nums) {
+        if (nums == null) return 0;
+        if (nums.length == 1) return nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i - 1] > nums[i]) {
+                return nums[i];
+            }
+        }
+        return nums[0];
+    }
+
+    /**
+     * use the way that fast search.
+     * I reference other people.
+     * 就是由上一题改变部分演变而来的!You must to learn versatility。
+     *
+     * <strong>result of test:</strong><br/>
+     * 189 / 189 test cases passed
+     * Status: Accepted
+     * Runtime: 1 ms, bit 4.88%
+     *
+     * @param nums
+     * @return
+     */
+    public int findMin4(int[] nums) {
+        if (nums == null) return 0;
+
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low < high && nums[low] >= nums[high]) {
+            int mid = (low + high) >> 1;
+            if (nums[low] > nums[mid]) {
+                high = mid;
+            } else if (nums[low] == nums[mid]) {
+                low++;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return nums[low];
+    }
+}
---
+++ question198_House_Robber/HouseRobber.java
@@ -0,0 +1,69 @@
+package zhang.algorithm.leetcode.question198_House_Robber;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: zhang_MacPro
+ * Date: 16/7/13
+ * Time: 上午10:45
+ * To change this template use File | Settings | File Templates.
+ */
+public class HouseRobber {
+    /**
+     * use dynamic programming
+     * <p>
+     * <strong>result of test:</strong><br/>
+     * 69 / 69 test cases passed
+     * Status: Accepted
+     * Runtime: 0 ms, bit 41.05%
+     * <p>
+     * 通过分析我们可以知道从前往后最后两个其中最大的就是能抢到的最大的钱
+     * 而我们从前往后走, 因为相邻两个不能一起走, 所以要么走两步,要么走三步。
+     * 不能走四步了,如果走四步,那么完全可以把第二步的也给包含了,这样不是抢更多么?
+     * 所以……如下所示
+     *
+     * @param nums
+     * @return
+     */
+    public int rob(int[] nums) {
+        if (nums == null || nums.length < 1) return 0;
+        if (nums.length == 1) return nums[0];
+
+        int[] max = new int[nums.length];
+        max[0] = nums[0];
+        max[1] = nums[1];
+        if (nums.length > 2)
+            max[2] = nums[0] + nums[2];
+
+        for (int i = 3; i < nums.length; i++) {
+            max[i] = Math.max(max[i - 3], max[i - 2]) + nums[i];
+        }
+
+        return Math.max(max[nums.length - 2], max[nums.length - 1]);
+    }
+
+    /**
+     * another dynamic programming method.
+     * 递推关系为maxV[i] = max(maxV[i-2]+num[i], maxV[i-1])
+     * this way is like more normal and perfect !!
+     * <p>
+     * And the result is the same as last way !!
+     *
+     * @param nums
+     * @return
+     */
+    public int rob2(int[] nums) {
+        if (nums == null || nums.length < 1) return 0;
+        if (nums.length == 1) return nums[0];
+
+        int len = nums.length;
+        int[] max = new int[len];
+
+        max[0] = nums[0];
+        max[1] = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < len; i++) {
+            max[i] = Math.max(max[i - 2] + nums[i], max[i - 1]);
+        }
+
+        return max[len - 1];
+    }
+}
