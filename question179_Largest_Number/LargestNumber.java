7ae58f9f631b0c3cf975bff2310d94fc7e286872
jiahua <1094006351@qq.com>
2016-07-13 22:34:28 +0800

    新建code179

---
+++ question179_Largest_Number/LargestNumber.java
@@ -0,0 +1,21 @@
+package zhang.algorithm.leetcode.question179_Largest_Number;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: zhang_MacPro
+ * Date: 16/7/13
+ * Time: 下午10:02
+ * To change this template use File | Settings | File Templates.
+ */
+public class LargestNumber {
+    /**
+     * this problem I think that can use the similar way that radix sort.
+     * But I can not change it to fit this problem.
+     *
+     * @param nums
+     * @return
+     */
+    public String largestNumber(int[] nums) {
+        return "";
+    }
+}
