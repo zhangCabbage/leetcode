9c167dce76e2f6936cf115c25e147e4a57499d98
jiahua <1094006351@qq.com>
2016-08-28 22:30:23 +0800

    update code 384

--- question384_Shuffle_Array/ShuffleArray.java
+++ question384_Shuffle_Array/ShuffleArray.java
@@ -3,47 +3,128 @@ package zhang.algorithm.leetcode.question384_Shuffle_Array;
 
 import zhang.algorithm.modelUtil.Array.ArrayTool;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 /**
  * Created by IntelliJ IDEA.
  * User: jiahua_MacPro
  * Date: 16/8/27
  * Time: 下午9:13
  * To change this template use File | Settings | File Templates.
+ * 1) first time
+ * this problem I try to use generate a random num then find the permutation of this array
+ * but it is Time Limit Exceeded
+ * 2) second time
+ * then I want to generate all permutation and storage those result in a two dimen array
+ * but it is Memory Limit Exceeded in the case [1,2,3,4,5,6,7,8,9,10,11,12]
  */
 public class ShuffleArray {
 
-    private int[][] alls = null;
-    private int facNum = 0;
+//    private int[][] alls = null;
+//    private int facNum = 0;
+//
+//    public ShuffleArray(int[] nums) {
+//        facNum = factorial(nums.length);
+//        this.alls = new int[facNum][nums.length];
+//
+//        for(int i=0; i<facNum; i++){
+//            int[] shuffleNums = new int[nums.length];
+//            System.arraycopy(nums, 0, shuffleNums, 0, nums.length);
+//            alls[i] = shuffleNums;
+//
+//            shuffle(nums);
+//        }
+//    }
+//
+//    /**
+//     * Resets the array to its original configuration and return it.
+//     */
+//    public int[] reset() {
+//        return this.alls[0];
+//    }
+//
+//    /**
+//     * Returns a random shuffling of the array.
+//     * this is ok, but to many shuffle handle is Time Limit Exceeded
+//     */
+//    public int[] shuffle() {
+//        int random = (int) (Math.random() * facNum); //0 - facNum-1
+//
+//        return this.alls[random];
+//    }
+
+    //---------------------------------------------------------------
+    //other method
+    //---------------------------------------------------------------
+    private int[] oriNums;
 
     public ShuffleArray(int[] nums) {
-        facNum = factorial(nums.length);
-        this.alls = new int[facNum][nums.length];
-
-        for(int i=0; i<facNum; i++){
-            int[] shuffleNums = new int[nums.length];
-            System.arraycopy(nums, 0, shuffleNums, 0, nums.length);
-            alls[i] = shuffleNums;
-
-            shuffle(nums);
-        }
+        this.oriNums = nums;
     }
 
     /**
      * Resets the array to its original configuration and return it.
      */
     public int[] reset() {
-        return this.alls[0];
+        return this.oriNums;
     }
 
     /**
      * Returns a random shuffling of the array.
-     * this is ok, but to many shuffle handle is Time Limit Exceeded
+     * <p>
+     * <strong>result of test:</strong>
+     * 10 / 10 test cases passed
+     * Status: Accepted
+     * Runtime: 319 ms
      */
     public int[] shuffle() {
-        int random = (int) (Math.random() * facNum); //0 - facNum-1
+        Random random = new Random();
+        List<Integer> list = new ArrayList<>();
+        for (int num : oriNums) {
+            list.add(num);
+        }
 
-        return this.alls[random];
+        int[] shuffleNums = new int[oriNums.length];
+        int size = list.size();
+
+        for (int i = 0; i < size; i++) {
+            int index = random.nextInt(list.size());
+            shuffleNums[i] = list.get(index);
+            list.remove(index);
+        }
+
+        return shuffleNums;
     }
+
+    /**
+     * the new method to get the random array
+     * recursive the size of array times to swap the random index and num from size-1 to 0
+     * <p>
+     * <strong>result of test:</strong>
+     * 10 / 10 test cases passed
+     * Status: Accepted
+     * Runtime: 301 ms
+     *
+     * [mathematical entity](http://www.dionysia.org/html/entities/symbols.html)
+     *
+     * @return
+     */
+    public int[] shuffle2() {
+        Random random = new Random();
+        int size = oriNums.length;
+        int[] shuffleNums = new int[size];
+        System.arraycopy(this.oriNums, 0, shuffleNums, 0, size);
+
+        while (size > 0) {
+            int index = random.nextInt(size);
+            swap(shuffleNums, index, --size);
+        }
+
+        return shuffleNums;
+    }
+
 
     public int factorial(int n) {
         //IntMath.factorial(n)
