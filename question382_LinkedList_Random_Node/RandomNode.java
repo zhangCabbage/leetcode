3ec5311c7a30bcb327a541741598bc1c8f6f6b7e
jiahua <1094006351@qq.com>
2016-09-17 16:57:20 +0800

    完成code 382

--- question382_LinkedList_Random_Node/RandomNode.java
+++ question382_LinkedList_Random_Node/RandomNode.java
@@ -2,6 +2,8 @@ package zhang.algorithm.leetcode.question382_LinkedList_Random_Node;
 
 import zhang.algorithm.modelUtil.List.ListNode;
 
+import java.util.Random;
+
 /**
  * Created by IntelliJ IDEA.
  * User: jiahua_MacPro
@@ -10,24 +12,40 @@ import zhang.algorithm.modelUtil.List.ListNode;
  * To change this template use File | Settings | File Templates.
  */
 public class RandomNode {
+    ListNode head;
+    Random random;
 
     /**
      * @param head The linked list's head.
      *             Note that the head is guaranteed to be not null, so it contains at least one node.
      */
     public RandomNode(ListNode head) {
-
+        this.head = head;
+        this.random = new Random();
     }
 
     /**
      * Returns a random node's value.
      * linked list is extremely large and its length is unknown, How to do?
      * the hint tag is Reservoir Sampling
+     * <p>
+     * 7 / 7 test cases passed
+     * Status: Accepted
+     * Runtime: 142 ms
      *
      * @return
      */
     public int getRandom() {
-        return 0;
+        ListNode curNode = this.head;
+        int count = 0;
+        int res = 0;
+        while (curNode != null) {
+            if (random.nextInt(++count) == 0) {
+                res = curNode.val;
+            }
+            curNode = curNode.next;
+        }
+        return res;
     }
 
     public static void main(String[] args) {
