1c98fa25f58b207313cdd91c06fc5c6f09a18e04
jiahua <1094006351@qq.com>
2016-09-19 23:12:43 +0800

    add code 400

---
+++ question400_Nth_Digit/NthDigit.java
@@ -0,0 +1,26 @@
+package zhang.algorithm.leetcode.question400_Nth_Digit;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: jiahua_MacPro
+ * Date: 16/9/19
+ * Time: 下午11:02
+ * To change this template use File | Settings | File Templates.
+ */
+public class NthDigit {
+    /**
+     * @param n
+     * @return
+     */
+    public int findNthDigit(int n) {
+
+        
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        NthDigit test = new NthDigit();
+        int n = 12;
+        System.out.println(test.findNthDigit(n));
+    }
+}
