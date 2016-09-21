d051a477cfb06fa37d81047482d8be8f2b70b556
jiahua <1094006351@qq.com>
2016-09-22 00:13:10 +0800

    update code 401

--- question401_Binary_Watch/BinaryWatch.java
+++ question401_Binary_Watch/BinaryWatch.java
@@ -83,9 +83,70 @@ public class BinaryWatch {
         }
     }
 
+    //-----------------------------------------------------------------
+    //second method
+    //-----------------------------------------------------------------
+
+    /**
+     * nice
+     *
+     * @param num
+     * @return
+     */
+    public List<String> readBinaryWatch2(int num) {
+        List<String> times = new ArrayList<>();
+        for (int h = 0; h < 12; h++) {
+            for (int m = 0; m < 60; m++) {
+                if (Integer.bitCount((h << 6) + m) == num)
+                    times.add(String.format("%d:%02d", h, m));
+            }
+        }
+        return times;
+    }
+
+    //-----------------------------------------------------------------
+    //third method
+    //-----------------------------------------------------------------
+    String[][] hour = {
+            {"0"},
+            {"1", "2", "4", "8"},
+            {"3", "5", "6", "9", "10"},
+            {"7", "11"}
+    };
+    String[][] minute = {
+            {"00"}, //1
+            {"01", "02", "04", "08", "16", "32"}, //6
+            {"03", "05", "06", "09", "10", "12", "17", "18", "20", "24", "33", "34", "36", "40", "48"}, //15
+            {"07", "11", "13", "14", "19", "21", "22", "25", "26", "28", "35", "37", "38", "41", "42", "44", "49", "50", "52", "56"}, //20
+            {"15", "23", "27", "29", "30", "39", "43", "45", "46", "51", "53", "54", "57", "58"}, //14
+            {"31", "47", "55", "59"} //4
+    };
+
+    /**
+     * what the fuck!
+     *
+     * @param num
+     * @return
+     */
+    public List<String> readBinaryWatch3(int num) {
+        List<String> ret = new ArrayList();
+        for (int i = 0; i <= 3 && i <= num; i++) {
+            if (num - i <= 5) { //!
+                for (String str1 : hour[i]) {
+                    for (String str2 : minute[num - i]) {
+                        ret.add(str1 + ":" + str2);
+                    }
+                }
+            }
+        }
+        return ret;
+    }
+
     public static void main(String[] args) {
         BinaryWatch test = new BinaryWatch();
-        int n = 2;
+        int n = 1;
         System.out.println(test.readBinaryWatch(n));
+        System.out.println(test.readBinaryWatch2(n));
+        System.out.println(test.readBinaryWatch3(n));
     }
 }
