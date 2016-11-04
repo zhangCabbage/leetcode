25c4dc454e747de0653d131f3ce15ed828a980f9
jiahua <1094006351@qq.com>
2016-11-04 21:01:35 +0800

    add code 309 and update code 377

---
+++ question309_Best_Buy_Sell_Stock_Cooldown/BestBuySellStockCooldown.java
@@ -0,0 +1,26 @@
+package zhang.algorithm.leetcode.question309_Best_Buy_Sell_Stock_Cooldown;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: jiahua_MacPro
+ * Date: 16/11/4
+ * Time: 下午8:05
+ * To change this template use File | Settings | File Templates.
+ */
+public class BestBuySellStockCooldown {
+    /**
+     * the tag of this problem is dynamic programming.
+     *
+     * @param prices
+     * @return
+     */
+    public int maxProfit(int[] prices) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        BestBuySellStockCooldown test = new BestBuySellStockCooldown();
+        int[] prices = {};
+        System.out.println(test.maxProfit(prices));
+    }
+}
--- question377_Combination_Sum_IV/CombinationSumIV.java
+++ question377_Combination_Sum_IV/CombinationSumIV.java
@@ -36,6 +36,110 @@ public class CombinationSumIV {
         return count;
     }
 
+    //----------------------------------------------------------------------
+    //针对动态规划的问题, 如何一步步的深入分析, 最终找到子问题, 并解决?
+    //----------------------------------------------------------------------
+
+    /**
+     * 1、仍使用递归的方法, 解决此问题, 做进一步改进, 不需要使用排序
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int combinationSum4_2(int[] nums, int target) {
+        int res = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (target >= nums[i]) res += combinationSum4_2(nums, target - nums[i]);
+        }
+        return res;
+    }
+
+    //----------------------------------------------------------------------
+    //----------------------------------------------------------------------
+
+    /**
+     * 2、针对递归中会针对某个值多次计算的问题, 使用备忘录的方式进行存储
+     * <p>
+     * 17 / 17 test cases passed.
+     * Status: Accepted
+     * Runtime: 1 ms, bit 93.01%
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int combinationSum4_3(int[] nums, int target) {
+        int[] map = new int[target + 1];
+        Arrays.fill(map, -1);  //why use -1, but use 0, because -1 means not use, 0 means can make
+        map[0] = 1;
+        return helper(nums, target, map);
+    }
+
+    private int helper(int[] nums, int target, int[] map) {
+        if (map[target] != -1) return map[target];
+        int res = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (target >= nums[i])
+                res += helper(nums, target - nums[i], map);
+        }
+
+        map[target] = res;
+        return res;
+    }
+
+    //----------------------------------------------------------------------
+    //----------------------------------------------------------------------
+
+    /**
+     * 3、动态规划解决当前问题
+     * <p>
+     * 17 / 17 test cases passed.
+     * Status: Accepted
+     * Runtime: 6 ms, bit 21.56%
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int combinationSum4_4(int[] nums, int target) {
+        int[] dp = new int[target + 1];
+        dp[0] = 1;
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (i >= nums[j])
+                    dp[i] += dp[i - nums[j]];
+            }
+        }
+        return dp[target];
+    }
+
+    //----------------------------------------------------------------------
+    //----------------------------------------------------------------------
+
+    /**
+     * 4、因为动态规划解决问题, 常见时间复杂度为O(n^2), 这里可以采用排序 nums 来改进动态规划
+     * <p>
+     * 17 / 17 test cases passed.
+     * Status: Accepted
+     * Runtime: 4 ms, bit 70.53%
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int combinationSum4_5(int[] nums, int target) {
+        int[] dp = new int[target + 1];
+        dp[0] = 1;
+        Arrays.sort(nums);
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[j] > i) break;
+                else dp[i] += dp[i - nums[j]];
+            }
+        }
+        return dp[target];
+    }
 
     public static void main(String[] args) {
         CombinationSumIV test = new CombinationSumIV();
