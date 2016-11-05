7126c4dbe32f1908b4f8b2792bc3d76701b226aa
jiahua <1094006351@qq.com>
2016-11-05 16:59:16 +0800

    add code 421

---
+++ question421_Max_XOR_Two_Num_in_Array/MaxXORTwoNumArray.java
@@ -0,0 +1,27 @@
+package zhang.algorithm.leetcode.question421_Max_XOR_Two_Num_in_Array;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: jiahua_MacPro
+ * Date: 16/11/5
+ * Time: 下午4:45
+ * To change this template use File | Settings | File Templates.
+ */
+public class MaxXORTwoNumArray {
+    /**
+     * (https://leetcode.com/problems/maximum-xor-of-two-numbers-in-an-array/)
+     * How should I do to solve this problem?
+     *
+     * @param nums
+     * @return
+     */
+    public int findMaximumXOR(int[] nums) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        MaxXORTwoNumArray test = new MaxXORTwoNumArray();
+        int[] nums = {};
+        System.out.println(test.findMaximumXOR(nums));
+    }
+}
