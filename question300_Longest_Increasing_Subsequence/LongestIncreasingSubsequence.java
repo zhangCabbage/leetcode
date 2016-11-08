570f15ed6cab7c99b70d931031be32d9a6e84b77
jiahua <1094006351@qq.com>
2016-11-08 20:24:58 +0800

    add code 300

---
+++ question300_Longest_Increasing_Subsequence/LongestIncreasingSubsequence.java
@@ -0,0 +1,28 @@
+package zhang.algorithm.leetcode.question300_Longest_Increasing_Subsequence;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: jiahua_MacPro
+ * Date: 16/11/8
+ * Time: 下午8:21
+ * To change this template use File | Settings | File Templates.
+ * <p>
+ * 16/11/08 20:22
+ * 今天状态不好, 但是为了维持github上连击的状态, 我无耻的新建了一个问题文件, 但是并没有做.
+ * 要求时间复杂度能达到 O(n * logN)
+ * <p>
+ * (https://leetcode.com/problems/longest-increasing-subsequence/)
+ */
+public class LongestIncreasingSubsequence {
+    /**
+     * @param nums
+     * @return
+     */
+    public int lengthOfLIS(int[] nums) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        LongestIncreasingSubsequence test = new LongestIncreasingSubsequence();
+    }
+}
