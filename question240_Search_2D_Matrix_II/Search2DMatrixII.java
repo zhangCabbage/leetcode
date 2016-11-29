d778e5bcec75700c2a1081b45fa524c423311a43
jiahua <1094006351@qq.com>
2016-11-29 22:43:32 +0800

    add code 367 and update code 240

--- question240_Search_2D_Matrix_II/Search2DMatrixII.java
+++ question240_Search_2D_Matrix_II/Search2DMatrixII.java
@@ -8,20 +8,59 @@ package zhang.algorithm.leetcode.question240_Search_2D_Matrix_II;
  * To change this template use File | Settings | File Templates.
  * =>
  * https://leetcode.com/problems/search-a-2d-matrix-ii/
+ * =>
+ * update
+ * Date: 16/11/29
+ * Time: 下午9:30
  */
 public class Search2DMatrixII {
     /**
-     * o(m * n) => o(m * log(n)) => ?
+     * o(m * log(n)) => 还能否继续优化
+     * <p>
+     * 127 / 127 test cases passed.
+     * Status: Accepted
+     * Runtime: 14 ms, bit 47.53%
      *
      * @param matrix
      * @param target
      * @return
      */
     public boolean searchMatrix(int[][] matrix, int target) {
-        return true;
+        int m = matrix.length;
+        for (int i = 0; i < m; i++) {
+            if (binarySearch(matrix[i], target)) return true;
+        }
+        return false;
+    }
+
+    private boolean binarySearch(int[] array, int target) {
+        int l = 0, r = array.length - 1;
+        while (l <= r) {
+            int mid = l + (r - l >> 1);
+            if (target > array[mid]) l = mid + 1;
+            else if (target < array[mid]) r = mid - 1;
+            else return true;
+        }
+        return false;
+    }
+
+    /**
+     * @param matrix
+     * @param target
+     * @return
+     */
+    public boolean searchMatrix2(int[][] matrix, int target) {
+        int m = matrix.length;
+        for (int i = 0; i < m; i++) {
+            if (binarySearch(matrix[i], target)) return true;
+        }
+        return false;
     }
 
     public static void main(String[] args) {
         Search2DMatrixII test = new Search2DMatrixII();
+        int[][] matrix = {{-5}};
+        int target = -2;
+        System.out.println(test.searchMatrix(matrix, target));
     }
 }
---
+++ question367_Valid_Perfect_Square/ValidPerfectSquare.java
@@ -0,0 +1,60 @@
+package zhang.algorithm.leetcode.question367_Valid_Perfect_Square;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: jiahua_MacPro
+ * Date: 16/11/29
+ * Time: 下午9:53
+ * To change this template use File | Settings | File Templates.
+ */
+public class ValidPerfectSquare {
+    /**
+     * 采用导数来进行查找, 比二分查找更快
+     * ==>
+     * 67 / 67 test cases passed.
+     * Status: Accepted
+     * Runtime: 0 ms, bit 30.31%
+     *
+     * @param num
+     * @return
+     */
+    public boolean isPerfectSquare(int num) {
+        int pre = num;
+        while (true) {
+            if (pre == 0) return false;
+            int cur = (int) ((1.0 * pre * pre + num) / (2.0 * pre));
+            if (cur * cur == num) return true;
+            if (cur >= pre) return false;
+            pre = cur;
+        }
+    }
+
+    /**
+     * 采用二分法来进行查找
+     * ==>
+     * 67 / 67 test cases passed.
+     * Status: Accepted
+     * Runtime: 1 ms, bit 15.18%
+     *
+     * @param num
+     * @return
+     */
+    public boolean isPerfectSquare2(int num) {
+        int l = 1, r = num;
+        while (l <= r) {
+            int mid = l + (r - l >> 1);
+            long tmp = 1L * mid * mid;
+            if (tmp < num) l = mid + 1;
+            else if (tmp > num) r = mid - 1;
+            else return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        ValidPerfectSquare test = new ValidPerfectSquare();
+        int num = 808201;  //899
+        System.out.println(test.isPerfectSquare(num));
+        System.out.println(test.isPerfectSquare2(num));
+    }
+}
