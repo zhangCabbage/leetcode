7adab2a8039183c87873ff7e71acbdd19762a0df
jiahua <1094006351@qq.com>
2016-12-02 11:45:52 +0800

    update code 10

--- question190_Reverse_Bits/ReverseBits.java
+++ question190_Reverse_Bits/ReverseBits.java
@@ -61,7 +61,7 @@ public class ReverseBits {
      * @return
      */
     public int reverseBits3(int n) {
-        n = ((n & 0x55555555) << 1) | (((n & 0xAAAAAAAA) >> 1) & 0x7FFFFFFF);
+        n = ((n & 0x55555555) << 1) | (((n & 0xAAAAAAAA) >> 1) & 0x7FFFFFFF); //防止负号移位
         n = ((n & 0x33333333) << 2) | (((n & 0xCCCCCCCC) >> 2) & 0x3FFFFFFF);
         n = ((n & 0x0F0F0F0F) << 4) | (((n & 0xF0F0F0F0) >> 4) & 0x0FFFFFFF);
         n = ((n & 0x00FF00FF) << 8) | (((n & 0xFF00FF00) >> 8) & 0x00FFFFFF);
